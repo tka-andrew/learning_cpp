a9c6357021884c0fe6270b11fefa692450d4a663
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-09-09 23:52:52 +0800

    added mutex_lock_unlock_example

---
+++ src/mutex_lock_unlock_example.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <mutex>
+
+// for this example, you need to create a file called dummy.txt and put a number in it
+
+int userCount = 0;
+std::mutex m;
+
+void registerUser() {
+    m.lock();
+    std::fstream db;
+    db.open("dummy.txt", std::ios::in); // open for read
+    int count = 0;
+    while (db >> count) {
+        userCount = count + 1;
+    }
+    db.close();
+    db.open("dummy.txt", std::ios::out | std::ios::trunc); // open to update file
+    db << userCount;
+    db.close();
+    m.unlock();
+}
+
+int main() {
+    std::thread registrationCounter1(registerUser);
+    std::thread registrationCounter2(registerUser);
+    std::thread registrationCounter3(registerUser);
+    std::thread registrationCounter4(registerUser);
+
+    registrationCounter1.join();
+    registrationCounter2.join();
+    registrationCounter3.join();
+    registrationCounter4.join();
+
+    // even though there are 4 threads here, but the end result will be less than 4 due to race condition
+    std::cout<<"userCount: "<<userCount<<'\n';
+    return 0;
+}
